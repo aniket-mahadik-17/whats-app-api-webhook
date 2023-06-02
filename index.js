7045da0d8baf9a14ce2023077f5e8e6f571b6dc7
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2023-06-02 18:16:11 +0530

    Update by adding new logs in index.js

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ app.get("/webhook",(req,res)=>{
 app.post("/webhook",(req,res)=>{ //i want some 
 
     let body_param=req.body;
-
+    
+    console.log("----------------------------------------Messages-----------------------------------------------------------");
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
@@ -47,10 +48,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
-
-               console.log("phone number "+phon_no_id);
-               console.log("from "+from);
-               console.log("boady param "+msg_body);
+               let userName = req.body.entry[0].changes[0].value.contacts[0].profile.name;
 
                axios({
                    method:"POST",
@@ -59,7 +57,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                        messaging_product:"whatsapp",
                        to:from,
                        text:{
-                           body:"Hi.. I'm Prasath, your message is "+msg_body
+                           body:`Hello ${userName}`
                        }
                    },
                    headers:{

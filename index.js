a454ec4fa5779f772d234f1bf74a95f789eae89e
aniket-mahadik-17 <92512266+aniket-mahadik-17@users.noreply.github.com>
2023-06-05 20:11:49 +0530

    Updated filtered data index.js

--- index.js
+++ index.js
@@ -58,13 +58,14 @@ app.post("/webhook",(req,res)=>{ //i want some
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
                let userName = req.body.entry[0].changes[0].value.contacts[0].profile.name;
                let timestamp= body_param.entry[0].changes[0].value.messages[0].timestamp;
+               let msgId= body_param.entry[0].changes[0].value.messages[0].id;
             
             //socket.io connection
              io.on("connection", (socket) => {
               socket.emit("originaldata", JSON.stringify(body_param,null,2));
              });
             io.on("connection", (socket) => {
-              socket.emit("filtereddata", {phon_no_id,from,msg_body,userName,timestamp});
+              socket.emit("filtereddata", {msgId,from,msg_body,userName,timestamp});
              });
 
                axios({
